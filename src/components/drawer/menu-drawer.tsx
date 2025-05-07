802d8932139cba7fab3e5a57d41bf7481e93c18b
15077693d <oscaryiu.lapsang@gmail.com>
2025-05-07 21:03:00 +0800

    feat: implement state management in MenuDrawer to control drawer visibility and close on content click

--- src/components/drawer/menu-drawer.tsx
+++ src/components/drawer/menu-drawer.tsx
@@ -8,16 +8,27 @@ import { Link } from "@/i18n/navigation";
 import { ROUTE_INFOS } from "@/lib/constants/route";
 import { Menu } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
 
 export function MenuDrawer() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleContentClick = () => {
+    // Close the drawer when content is clicked
+    setIsOpen(false);
+  };
+
   const tRoute = useTranslations("route");
   return (
-    <Drawer>
+    <Drawer open={isOpen} onOpenChange={setIsOpen}>
       <DrawerTrigger className=" cursor-pointer">
         <Menu width={24} height={24} />
       </DrawerTrigger>
       <DrawerTitle className="hidden">Menu</DrawerTitle>
-      <DrawerContent className="min-h-[250px] overflow-auto p-6">
+      <DrawerContent
+        className="min-h-[250px] overflow-auto p-6"
+        onClick={handleContentClick}
+      >
         {Object.entries(ROUTE_INFOS)
           .filter(([_, info]) => info.isShowInDrawer)
           .map(([key, info]) => (
