1354d29d165a15ab379cbdf1e158ab868eb541f8
15077693d <oscaryiu.lapsang@gmail.com>
2025-06-13 11:47:41 +0800

    chore: remove GitHub provider from authentication configuration

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -3,7 +3,6 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
-import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 import {
@@ -51,7 +50,6 @@ export const authConfig = {
   providers: [
     DiscordProvider,
     Google,
-    GitHub,
     ...(process.env.VERCEL_ENV === "production"
       ? []
       : [
