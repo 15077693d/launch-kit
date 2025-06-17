22a3fd7171b44521271fb556ded63837f4787311
15077693d <oscaryiu.lapsang@gmail.com>
2025-06-17 12:26:27 +0800

    feat: add dummy router with protected query returning greeting message

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,21 @@
-import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/server/api/trpc";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  dummy: createTRPCRouter({
+    hello: protectedProcedure.query(() => {
+      return "Hello, world!";
+    }),
+  }),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
